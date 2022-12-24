63ddab2fd2563b037031fb3c8c78a78ed166d121
Myrzaim <mikosh9767@gmail.com>
2022-12-25 01:26:37 +0600

    pagination done

--- src/components/Main/NewBooks/NewBooks.jsx
+++ src/components/Main/NewBooks/NewBooks.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { bookContext } from '../../contexts/bookContext';
 import Books from '../Books';
 import next from '../../../images/icons/nextslider.png';
@@ -6,20 +6,22 @@ import back from '../../../images/icons/backslider.png';
 import './newbooks.scss';
 
 function NewBooks() {
-  const { newBook,newBooks } = useContext(bookContext);
+  const { newBook, newBooks,totalPages } = useContext(bookContext);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
-    newBook();
-  }, []);
-  const arr = [1, 2, 3, 4, 5];
+    newBook(page);
+  }, [page]);
+
+  
   return (
     <div className='books'>
       <h2 className='books__title'>Новинки</h2>
       <ul className='books__items'>
         {newBooks ? newBooks.map((item) => <Books obj={item} />) : null}</ul>
-        <img src={next} alt='next' className='next' />
-      <img src={back} alt='back' className='back' />
-    </div>
+      {page+1 == totalPages ? null :<img src={next} alt='next' className='next' onClick={(e) => setPage(page+1)} />}
+      {page > 0 ? <img src={back} alt='back' className='back' onClick={(e) => setPage(page-1)} /> : null}
+    </div>  
   );
 }
 
